45a559d0870b400bf6a5a621ab37de2a39579d3e
Sung Hyun Hong (Jenny) <passionnhope0110@gmail.com>
2021-12-08 21:14:30 +0900

    Add files via upload

---
+++ 08_02.py
@@ -0,0 +1,14 @@
+total = 0
+count = 0
+while True:
+    inp = input('Enter a number: ')
+    if inp == 'done':
+        break
+    value = float(inp)
+    total = total +value
+    count = count+1
+    average = total/count
+    print('averrage: ', average)
+
+#08_04
+han = open('')
